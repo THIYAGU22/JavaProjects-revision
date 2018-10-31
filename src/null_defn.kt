bca997543b4cecf7030a746b21af1ed9043b8b06
THIYAGARAJAN <thiyagarajanravi22@gmail.com>
2018-10-31 15:54:32 +0530

    usage of null operator in kotlin

--- src/null_defn.kt
+++ src/null_defn.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>){
 
+    //in this example we can see about null
+
+    var str :  String = "thiyagarajan"
+    println("${str.length}")
+   // str = null --> you cant override the null value to variable str
+
+
+    //but you can override and pass the null value the trick is with ? with the data type
+    var name : String? ="john mathew"
+
+
+    println(name?.length)// ?. safe accessing operator
+    name = null // here no error will be displayed to the variable name
+    println("${name}") // but you cant check the length property here
 }
